95137aa9f2d2f26bbb6fd3c6dce4e92030f4a3c3
katya-voronova <katya@example.com>
2025-10-04 17:13:45 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,13 +18,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',  # :cite[2]
+    'corsheaders',
     'rest_framework',
     'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # :cite[2]:cite[5]
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
